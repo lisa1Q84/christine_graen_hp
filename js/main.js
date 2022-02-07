33e47757c6f203d3b0ae90c68f965cc0966857a4
lisafeigen@web.de <lisagerda@lisas-MacBook-Air.local>
2022-02-07 22:35:38 +0100

    add mini slider to index page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 //Followed instructions from https://medium.com/@adamwojnicki/lets-build-a-simple-responsive-navigation-bar-with-html-css-and-a-little-bit-of-vanilla-js-5abba28da32e
 
 // Selection of HTML objects
@@ -20,3 +21,38 @@ window.onscroll = () => {
   const div = document.querySelector('#navbarbg');
   if(this.scrollY <= 10) div.className= ''; else div.className = 'scroll';
 };
+
+// Slider 
+
+let slider = tns({
+	container : ".my-slider",
+	"slideBy" : "1",
+	"speed" : 400,
+	"nav" : false,
+	autoplay: true,
+	controls: false,
+	autoplayButtonOutput: false,
+	responsive: {
+		1600: {
+			items: 4,
+			gutter: 20
+		},
+		1024: {
+			items: 3,
+			gutter: 20
+		},
+		768: {
+			items: 2,
+			gutter: 20
+		},
+		480: {
+			items: 1
+		}
+	}
+
+
+
+
+
+
+})
